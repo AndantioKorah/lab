f1123cda15b56c01e10757b9494ec92af91ac982
youri <torehyouri@gmail.com>
2021-11-20 15:58:05 +0800

    pull

--- application/views/laporan/V_LaporanPendaftaranPerPasienExcel.php
+++ application/views/laporan/V_LaporanPendaftaranPerPasienExcel.php
@@ -20,6 +20,7 @@
             <th class="format_str text-center">NAMA PASIEN</th>
             <th class="format_str text-center">TGL. PENDAFTARAN</th>
             <th class="format_str text-center">NO. PENDAFTARAN</th>
+            <th class="format_str text-center">DOKTER PENGIRIM</th>
             <th class="format_str text-center">TOTAL TAGIHAN</th>
             <th class="format_str text-center">STATUS TAGIHAN</th>
         </thead>
@@ -30,6 +31,7 @@
                 <td class="format_str" style="text-align: left;"><?=$rs['nama_pasien']?></td>
                 <td class="format_str" style="text-align: center;"><?=formatDate($rs['tanggal_pendaftaran'])?></td>
                 <td class="format_str" style="text-align: center;"><?=strval($rs['nomor_pendaftaran'])?></td>
+                <td class="format_str" style="text-align: left;"><?=$rs['nama_dokter_pengirim']?></td>
                 <td class="format_str" style="text-align: left;"><?=formatCurrency($rs['total_tagihan'])?></td>
                 <td class="format_str" style="text-align: center;"><?=$rs['flag_active'] == 0 ? 'Dihapus' : $rs['status_tagihan']?></td>
             </tr>
